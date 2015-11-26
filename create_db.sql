1be18044fb0d0a3522e321ecbee6cf6d24af60e5
Daniel Kokott <dako@berlingskemedia.dk>
2015-11-26 10:12:06 +0100

    SQL INSERT for empty locations table

--- create_db.sql
+++ create_db.sql
@@ -28,6 +28,9 @@ CREATE TABLE IF NOT EXISTS `locations` (
 ) ENGINE=InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_danish_ci;
 CREATE INDEX `location_ident` ON `locations` (`ident`);
 
+INSERT INTO locations (ident, ident_int, name, areatype, type, parent_ident, votes_allowed, votes_made, votes_pct, votes_valid, votes_invalid_blank, votes_invalid_other, votes_invalid_total, votes_yes, votes_yes_pct, votes_no, votes_no_pct, status_code, status_text, updated_at, created_at)
+VALUES ('0', 0, 'Hele landet', 'L', 'HeleLandet', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Ikke startet', '2015-11-26 10:00:00', '2015-11-26 10:00:00');
+
 CREATE TABLE IF NOT EXISTS `newsticker` (
   `id` MEDIUMINT NOT NULL AUTO_INCREMENT,
   `tweet` VARCHAR(255) NOT NULL,
